d90fc75b96b2cf033e36fa211eba9904d164caa7
Artamananda <artamananda@gmail.com>
2023-01-02 22:38:18 +0700

    add timestamp

--- assets/js/read.js
+++ assets/js/read.js
@@ -2,7 +2,7 @@ const url = 'https://sheet2api.com/v1/WlmGDOXaS8W7/db-question';
 const options = {};
 Sheet2API.read(url, options).then(function(result){
     for(var i =0; i < result.length; i++){
-        $('#x').append('<div id="quest'+ i +'" class="bg-gray-200 rounded-2xl p-8 my-4 w-11/12 mx-auto">'+result[i].question+'</div>')
+        $('#x').append('<div id="quest'+ i +'" class="font-bold bg-gray-200 rounded-2xl p-8 my-4 w-11/12 mx-auto"><div class="font-light">'+result[i].timestamp+"</div>"+result[i].question+'</div>')
     }
 }, function(error){
     console.log(error);
--- assets/js/write.js
+++ assets/js/write.js
@@ -2,8 +2,9 @@ function isValid(inputField){
     if(inputField == ""){
         return false;
     }else{
+        var datetime = "[" + getDateTime() + "]";
         const url = 'https://sheet2api.com/v1/WlmGDOXaS8W7/db-question';
-        const newRowData = {"question": inputField};
+        const newRowData = {"timestamp": datetime, "question": inputField};
         const options = {};
         Sheet2API.write(url, options, newRowData).then(function(result){
             console.log(result);
@@ -12,4 +13,29 @@ function isValid(inputField){
         });
     }
     return true;
-} 
+}
+
+function getDateTime() {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = months[now.getMonth()]; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    } 
+    if(hour.toString().length == 1) {
+         hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }   
+    var dateTime = day+' '+month+' '+year+', '+hour+':'+minute+':'+second;   
+    return dateTime;
+}
