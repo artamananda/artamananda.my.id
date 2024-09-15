498290e48f03679478ad50ffee3e722ef3d9d0a3
Artamananda <artamananda@gmail.com>
2024-09-15 14:47:59 +0700

    adjust footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer style={{ margin: 20 }}>
+    <footer style={{ margin: 20, width: "70vw" }}>
       <div style={{ textAlign: "center" }}>
         {`Copyright © 2024 Artamananda v${process.env.APP_VERSION} All rights reserved`}
       </div>
