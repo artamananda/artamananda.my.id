f6895df4e33d7b6493d387e06e9500c90dd21db0
Artamananda <artamananda@gmail.com>
2024-09-16 17:10:46 +0700

    adjust image and icon

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
     APP_NAME: process.env.APP_NAME,
     APP_VERSION: process.env.APP_VERSION,
     BASE_API_URL: process.env.BASE_API_URL,
+    STORAGE_URL: process.env.STORAGE_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          process.env.STORAGE_URL.split("/")[
+            process.env.STORAGE_URL.split("/").length - 2
+          ],
+        port: "",
+        pathname: `/${process.env.STORAGE_URL.split("/").pop()}/**`,
+      },
+    ],
   },
 };
 
