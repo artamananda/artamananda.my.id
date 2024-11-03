01bc821a09757d0eb31e8f1385bc48e07ad0618c
Artamananda <artamananda@gmail.com>
2024-11-03 21:45:14 +0700

    adjust footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
         justifyContent: "space-between",
         alignItems: "center",
         padding: 10,
-        height: "100vh",
+        minHeight: "100vh",
       }}
     >
       <Toaster />
